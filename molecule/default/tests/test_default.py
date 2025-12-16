98ce20688bebcdb404038a39dba8c6ce87852ec9
Thomas Boerger <thomas@webhippie.de>
2025-12-16 21:45:52 +0100

    ci: add basic test for loki service

--- molecule/default/tests/test_default.py
+++ molecule/default/tests/test_default.py
@@ -6,5 +6,11 @@ testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
 ).get_hosts("all")
 
 
-def test_dummy(host):
-    assert True
+def test_running_and_enabled(host):
+    svc = host.service("loki")
+    assert svc.is_running
+    assert svc.is_enabled
+
+def test_docker_is_enabled(host):
+    docker = host.docker("loki")
+    assert docker.is_running
